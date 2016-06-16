8170e2319438193fcfea38f45638a9f9643cf393
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-06-16 12:40:51 +0100

    Adds some documentation

--- src/Crystal.jl
+++ src/Crystal.jl
@@ -5,7 +5,16 @@ using DataFrames: DataFrame, NA, DataArray, nrow, ncol
 typealias POSITIONS Array{Float64, 2}
 typealias CELL POSITIONS
 
-" All the information needed to specify a crystal structure "
+"""
+Specify a crystal structure
+
+A structure is constructed empty of atoms. Structure
+
+    structure = Structure([0 0.5 0.5; 0.5 0 0.5; 0.5 0.5 0], 2e0)
+    @assert abs(structure.scale - 2e0) < 1e-8
+
+    push!(structure, [0, 0, 0], 'Al'; charge=3)
+"""
 type Structure
   " 3x3 matrix specifying the translational symmetries of the structure "
   cell::CELL
@@ -25,6 +34,24 @@ end
 Base.length(s::Structure) = size(s.positions, 2)
 Base.ndims(s::Structure) = size(s.cell, 1)
 
+"""
+    Base.push!(structure::Structure, position, specie; <keyword arguments>)
+
+Add an atom to the structure. The `position` and associated `specie` are
+required arguments. Other properties can be added using keyword arguments.
+
+# Examples
+
+```julia
+push!(structure, [0, 0, 0], NA)
+push!(structure, [1, 2, 3], "Na"; shell="fruity")
+```
+
+The first line above adds an atom at the origin with a unidentified/missing
+atomic specie (`NA`). The second line adds a Sodium atom at position `[1 2 3]`,
+with an additional property `shell`. If other atoms in the structure have not
+specified this property, then their `shell` is set to missing (`NA`).
+"""
 function Base.push!(s::Structure, position, specie; kwargs...)
   s.positions = hcat(s.positions, position)
   # Add row to properties
