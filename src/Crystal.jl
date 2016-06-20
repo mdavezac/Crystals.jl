b751bbf3369fc16a655412967b2376d9c9e54534
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-06-20 20:41:46 +0100

    First attempts at atomic properties as dataframe
    Positions are included as Fixed size vectors.

--- src/Crystal.jl
+++ src/Crystal.jl
@@ -1,5 +1,149 @@
-module Crystal
+module Crystals
 
-# package code goes here
+using DataFrames: DataFrame, nrow, DataArray, ColumnIndex
+using FixedSizeArrays: FixedVectorNoTuple
+
+
+for s in 2:6
+  local name = Symbol("Position$(s)D")
+  local docstring = "Position for $s-dimensional crystals"
+  local code = begin
+    u = quote
+      $docstring
+      immutable $name{T <:Real} <: FixedVectorNoTuple{$s, T}
+      end
+    end
+    for (i, symb) in enumerate([:x, :y, :z, :u, :v, :w][1:s])
+      push!(u.args[4].args[3].args, :($symb::T))
+    end
+    u
+  end
+  eval(code)
+end
+
+" All acceptable types for positions "
+typealias PositionTypes Union{
+  Position2D, Position3D,
+  Position4D, Position5D, Position6D
+}
+
+const MAX_POSITION_SIZE = maximum([length(u) for u in PositionTypes.types])
+const MIN_POSITION_SIZE = minimum([length(u) for u in PositionTypes.types])
+function positions_type(x::Vector)
+  MIN_POSITION_SIZE ≤ length(x) ≤ MAX_POSITION_SIZE ||
+    error("Incorrect column vector size")
+  position_index = findfirst(u -> length(u) == length(x), PositionTypes.types)
+  PositionTypes.types[position_index]{eltype(x)}
+end
+
+# Add conversion rules from arrays
+for position_type in PositionTypes.types
+  @eval begin
+    function Base.convert{T <: Real}(::Type{Vector{$(position_type){T}}}, x::Matrix)
+      size(x, 1) == $(length(position_type)) || error("Incompatible vector size")
+      $(position_type){T}[$(position_type)(x[:, u]) for u in 1:size(x, 2)]
+    end
+  end
+end
+
+" Converts inputs to something a DataArray of Positions will understand "
+function Positions(x::Matrix)
+  MIN_POSITION_SIZE ≤ size(x, 1) ≤ MAX_POSITION_SIZE ||
+    error("Incorrect column vector size")
+  return convert(Vector{positions_type(x[:, 1])}, x)
+end
+Positions{T <: PositionTypes}(x::DataArray{T}) = x
+Positions{T <: PositionTypes}(x::Vector{T}) = x
+
+abstract AbstractCrystal
+
+"""
+Crystal structure
+
+Holds all data necessary to define a crystal structure.
+The cell and scale are held as fields.
+The atomic properties, including the positions, are held in a DataFrame.
+"""
+type Crystal{T} <: AbstractCrystal
+  """ Scale/unit of the positions and cell """
+  scale::T
+  """ Periodicity of the crystal structure """
+  cell::Matrix{T}
+  """ Atoms and atomic properties """
+  atoms::DataFrame
+
+  function Crystal(cell, scale, atoms::DataFrame)
+    size(cell, 1) == size(cell, 2) || error("Cell matrix is not square")
+    MIN_POSITION_SIZE ≤ size(cell, 1) ≤ MAX_POSITION_SIZE ||
+      error("Incorrect column vector size")
+    :position ∈ names(atoms) || nrow(atoms) == 0 ||
+      error("Input Dataframe has atoms without positions")
+    if nrow(atoms) == 0
+      atoms[:position] = Vector{positions_type(cell[:, 1])}()
+    else
+      eltype(atoms[:position]) <: PositionTypes ||
+        error("Positions do not have acceptable type")
+      size(cell, 1) == length(eltype(atoms[:position])) ||
+        error("Cell and position dimensionality do not match")
+    end
+    new(scale, cell, atoms)
+  end
+end
+
+function Crystal(cell::Matrix, scale=1::Real; kwargs...)
+  nkwargs = Tuple{Symbol, Any}[]
+  for i in 1:length(kwargs)
+    if kwargs[i][1] ≠ :position
+      push!(nkwargs, kwargs[i])
+      continue
+    end
+    position = Positions(kwargs[i][2])
+    size(cell, 1) == length(eltype(position)) ||
+      error("Dimensionality of cell and positions do not match")
+    push!(nkwargs, (:position, position))
+  end
+
+  atoms = DataFrame(; nkwargs...)
+  Crystal{eltype(cell)}(cell, scale, atoms)
+end
+
+function Crystal(cell::Matrix, columns::Vector{Any}, names::Vector{Symbol},
+                 scale=1::Real)
+  for (i, (value, name)) in enumerate(zip(columns[:], names[:]))
+    name == :position || continue
+
+    columns[i] = Positions(value)
+    size(cell, 1) == length(eltype(columns[i])) ||
+      error("Dimensionality of cell and positions do not match")
+  end
+
+  atoms = DataFrame(columns, names)
+  Crystal{eltype(cell)}(cell, scale, atoms)
+end
+
+# Forwards all indexing to the DataFrame
+Base.getindex(crystal::Crystal, col::ColumnIndex) = crystal.atoms[col]
+Base.getindex(crystal::Crystal, ::Colon) = crystal.atoms[:]
+Base.getindex(crystal::Crystal, cols::AbstractVector{ColumnIndex}) = crystal.atoms[cols]
+function Base.getindex{R <: Integer, C <: ColumnIndex}(crystal::Crystal,
+                                                       row::Union{R, AbstractVector{R}},
+                                                       col::Union{C, AbstractVector{C}})
+  crystal.atoms[row, col]
+end
+function Base.getindex{C <: ColumnIndex}(crystal::Crystal,
+                                         ::Colon,
+                                         col::Union{C, AbstractVector{C}})
+  crystal.atoms[:, col]
+end
+function Base.getindex{R <: Integer}(crystal::Crystal,
+                                     row::Union{R, AbstractVector{R}},
+                                     ::Colon)
+  crystal.atoms[row, :]
+end
+Base.getindex(crystal::Crystal, ::Colon, ::Colon) = crystal.atoms[:, :]
+Base.names(crystal::Crystal) = names(crystal.atoms)
+Base.endof(crystal::Crystal) = endof(crystal.atoms)
+
+export Crystal, Positions
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,53 @@
-using Crystal
-using Base.Test
+module CrystalTest
+using Crystals
+using FactCheck: @fact, facts, context, exitstatus, roughly, exactly
+using DataFrames: nrow, ncol, NA, DataArray
 
-# write your own tests here
-@test 1 == 1
+contains(x) = y -> x ∈ y
+
+facts("Positions") do
+  positions = Positions([1 2; 3 4])
+  @fact length(positions) --> 2
+  @fact positions[1] --> [1, 3]
+  @fact positions[2] --> [2, 4]
+end
+
+facts("Construction") do
+  context("Empty 2d") do
+    crystal = Crystal(eye(2), 2e0)
+    @fact nrow(crystal.atoms) --> 0
+    @fact names(crystal.atoms) --> [:position]
+  end
+
+  context("3d with only positions") do
+    crystal = Crystal(eye(3), position=transpose([1 1 1; 2 3 4]))
+    @fact nrow(crystal.atoms) --> 2
+    @fact names(crystal.atoms) --> [:position]
+    @fact crystal.atoms[1, :position] --> [1, 1, 1]
+    @fact crystal.atoms[2, :position].x --> 2
+    @fact crystal.atoms[2, :position].y --> 3
+    @fact crystal.atoms[2, :position].z --> 4
+  end
+
+  context("2d with positions and species") do
+    crystal = Crystal(eye(2), position=[1 1 1; 2 3 4], specie=["Al", "O", "O"])
+    @fact nrow(crystal.atoms) --> 3
+    @fact names(crystal.atoms) --> [:position, :specie]
+
+    # Index of position column depends on input
+    crystal = Crystal(eye(2), specie=["Al", "O", "O"], position=[1 1 1; 2 3 4])
+    @fact names(crystal.atoms) --> [:specie, :position]
+  end
+
+  context("4d, constructing via arguments") do
+    crystal = Crystal(eye(4), Any[["Al", "O"], [1 1; 2 2; 3 3; 4 4]],
+                      [:specie, :position])
+    @fact names(crystal.atoms) --> [:specie, :position]
+    @fact nrow(crystal.atoms) --> 2
+    @fact eltype(crystal.atoms[:position]) --> exactly(Crystals.Position4D{Int64})
+  end
+end
+
+
+exitstatus()
+end
