4733c2c8ae58a4a9f33900a48bae6eb765ebc8d6
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-08-13 13:57:24 +0100

    Gruber seems to work well enough

--- src/Gruber.jl
+++ src/Gruber.jl
@@ -33,13 +33,13 @@ end
 
 
 function n1_action(params::Vector, rinv::Matrix)
-  rinv *= [0 -1 0; -1 0 0; 0 0 -1]
+  rinv[:, :] = rinv * [0 -1 0; -1 0 0; 0 0 -1]
   params[1], params[2] = params[2], params[1]
   params[4], params[5] = params[5], params[4]
 end
 
 function n2_action(params::Vector, rinv::Matrix)
-  rinv *= [-1 0 0; 0 0 -1; 0 -1 0]
+  rinv[:, :] = rinv * [-1 0 0; 0 0 -1; 0 -1 0]
   params[2], params[3] = params[3], params[2]
   params[5], params[6] = params[6], params[5]
 end
@@ -48,7 +48,7 @@ function n3_action(params::Vector, rinv::Matrix; tolerance=default_tolerance)
   const i = params[4] ≤ -tolerance ? -1 : 1
   const j = params[5] ≤ -tolerance ? -1 : 1
   const k = params[6] ≤ -tolerance ? -1 : 1
-  rinv *= [i 0 0; 0 j 0; 0 0 k]
+  rinv[:, :] = rinv * [i 0 0; 0 j 0; 0 0 k]
   params[4:end] = abs(params[4:end])
 end
 
@@ -66,13 +66,13 @@ function n4_action(params::Vector, rinv::Matrix; tolerance=default_tolerance)
   elseif i * j * k == -1
     error("Internal error")
   end
-  rinv *= update
+  rinv[:, :] = rinv * update
   params[4:end] = -abs(params[4:end])
 end
 
 function n5_action(params::Vector, rinv::Matrix; tolerance=default_tolerance)
   const sign = params[4] > tolerance ? -1 : 1
-  rinv *= [1 0 0; 0 1 sign; 0 0 1]
+  rinv[:, :] = rinv * [1 0 0; 0 1 sign; 0 0 1]
   params[3] += params[2] + sign * params[4]
   params[4] += 2sign * params[2]
   params[5] += sign * params[6]
@@ -80,7 +80,7 @@ end
 
 function n6_action(params::Vector, rinv::Matrix; tolerance=default_tolerance)
   const sign = params[5] > tolerance ? -1 : 1
-  rinv *= [1 0 sign; 0 1 0; 0 0 1]
+  rinv[:, :] = rinv * [1 0 sign; 0 1 0; 0 0 1]
   params[3] += params[1] + sign * params[5]
   params[4] += sign * params[6]
   params[5] += 2sign * params[1]
@@ -88,19 +88,29 @@ end
 
 function n7_action(params::Vector, rinv::Matrix; tolerance=default_tolerance)
   const sign = params[6] > tolerance ? -1 : 1
-  rinv *= [1 sign 0; 0 1 0; 0 0 1]
+  rinv[:, :] = rinv * [1 sign 0; 0 1 0; 0 0 1]
   params[2] += params[1] + sign * params[6]
   params[4] += sign * params[5]
   params[6] += 2sign * params[1]
 end
 
 function n8_action(params::Vector, rinv::Matrix)
-    rinv *= [1 0 1; 0 1 1; 0 0 1]
-    params[3] += sum(params[1:2]) + sum(params[4:end])
-    params[4] += 2params[2] + params[6]
-    params[5] += 2params[1] + params[6]
+  rinv[:, :] = rinv * [1 0 1; 0 1 1; 0 0 1]
+  params[3] += sum(params[1:2]) + sum(params[4:end])
+  params[4] += 2params[2] + params[6]
+  params[5] += 2params[1] + params[6]
 end
 
+""" Determines Gruber cell of an input cell
+
+The Gruber cell is an optimal parameterization of a lattice, eg shortest
+cell-vectors and angles closest to 90 degrees.
+
+# Arguments
+* `cell::Matrix`: the input lattice cell-vectors. Cannot be singular.
+* `itermax::Int`: maximum number of iterations before bailing out
+* `tolerance::Real`: tolerance parameter when comparing real numbers
+"""
 function gruber(cell::Matrix; tolerance=default_tolerance, itermax=50, max_no_change=10)
   size(cell, 1) == size(cell, 2) || error("Matrix not rectangular")
   abs(det(cell)) > tolerance || error("Singular matrix");
@@ -109,6 +119,7 @@ function gruber(cell::Matrix; tolerance=default_tolerance, itermax=50, max_no_ch
     vcat(diag(metric), [2metric[2, 3], 2metric[1, 3], 2metric[1, 2]])
   rinv = eye(size(metric, 1))
   nochange, previous = 0, -params[1:3]
+  iteration::Int = 0
   for iteration in 1:itermax
     const ε = tolerance
     condition0 =
@@ -150,6 +161,7 @@ function gruber(cell::Matrix; tolerance=default_tolerance, itermax=50, max_no_ch
     ) || break
     n8_action(params, rinv)
   end
+  iteration == itermax && error("Reached end of iteration without converging")
   cell * rinv
 end
 
