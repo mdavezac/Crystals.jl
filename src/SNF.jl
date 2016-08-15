01c975fd557cd5ccdac7643efe732d26a77de170
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-08-15 20:55:25 +0100

    Adding pivot improvement

--- src/SNF.jl
+++ src/SNF.jl
@@ -1,118 +1,64 @@
 module SNF
 export smith_normal_form
 
-const default_tolerance = 1e-12
-
-function one_nonzero(vector::Array)
-  found = false
-  for v in vector
-    if v != 0
-      found && return false
-      found = true
-    end
+function choose_pivot!{T <: Integer}(left::Matrix{T}, smith::Matrix{T},
+                                     step::Integer, index::Integer=1)
+  while all(smith[:, index] .== 0)
+    index += 1
+    index ≤ size(smith, 2) || return 0;
   end
-  true
+  if smith[step, index] == 0
+    k = findfirst(x -> x ≠ 0, smith[:, index])
+    left[step, :], left[k, :] = left[k, :], left[step, :]
+    smith[step, :], smith[k, :] = smith[k, :], smith[step, :]
+  end
+  index
 end
 
-function check_nonzero(matrix::Matrix, index::Int)
-  found = false
-  for i in 1:size(matrix, 1)
-    if matrix[i, index] != 0
-      found && return false
-      found = true
-    end
+function improve_col_pivot!{T <: Integer}(left::Matrix{T}, smith::Matrix{T},
+                                          row::Integer, col::Integer)
+  @assert size(left, 1) == size(left, 2) == size(smith, 1) == size(smith, 2)
+  for k in 1:size(smith, 1)
+    smith[k, col] % smith[row, col] == 0 && continue
+
+    β, σ, τ = gcdx(smith[row, col], smith[k, col])
+    α = smith[row, col] / β
+    γ = smith[k, col] / β
+
+    Lp = eye(T, size(left, 1))
+    Lp[row, [row, k]] = [σ, τ]
+    Lp[k, [row, k]] = [-γ, α]
+
+    Lpinv = eye(T, size(left, 1))
+    Lpinv[row, [row, k]] = [α, -τ]
+    Lpinv[k, [row, k]] = [γ, σ]
+
+    left[:] = left * Lpinv
+    smith[:] = Lp * smith
   end
-  for i in 1:size(matrix, 2)
-    if i != index && matrix[index, i] != 0
-      found && return false
-      found = true
-    end
-  end
-  true
 end
 
-function get_min_max(sequence)
-  min, max = nothing, 1
-  for k in 2:length(sequence)
-    abs(sequence[k]) > abs(sequence[max]) && (max = k)
-    sequence[k] != 0 &&
-      (min === nothing || abs(sequence[k]) < abs(sequence[min])) &&
-      (min = k)
+function improve_row_pivot!{T <: Integer}(smith::Matrix{T}, right::Matrix{T},
+                                          row::Integer, col::Integer)
+  @assert size(right, 1) == size(right, 2) == size(smith, 1) == size(smith, 2)
+  for k in 1:size(smith, 1)
+    smith[row, k] % smith[row, col] == 0 && continue
+
+    β, σ, τ = gcdx(smith[row, col], smith[row, k])
+    α = smith[row, col] / β
+    γ = smith[row, k] / β
+
+    Rp = eye(T, size(right, 1))
+    Rp[[col, k], col] = [σ, τ]
+    Rp[[col, k], k] = [-γ, α]
+
+    Rpinv = eye(T, size(right, 1))
+    Rpinv[[col, k], col] = [α, -τ]
+    Rpinv[[col, k], k] = [γ, σ]
+
+    right[:] = Rpinv * right
+    smith[:] = smith * Rp
   end
-  @assert min === nothing || sequence[min] != 0
-  min, max
 end
 
-function check_same_magnitude(matrix::Matrix, index::Int)
-  minelem, maxelem = get_min_max(matrix[:, index])
-  @assert minelem !== nothing
-  abs(matrix[minelem, index]) != abs(matrix[maxelem, index]) &&
-      return minelem, maxelem
-  n0, n1 = countnz(matrix[maxelem, :]), countnz(matrix[minelem, :])
-  if n0 < n1 || (n0 == n1 && matrix[maxelem, index] < matrix[minelem, index])
-    return maxelem, minelem
-  end
-  minelem, maxelem
-end
-
-function smith_col_impl(left::Matrix, smith::Matrix, index::Int)
-  L, S = copy(left), copy(smith)
-  while !one_nonzero(S[:, index])
-    minelem, maxelem = check_same_magnitude(S, index)
-    const multiple = S[maxelem, index] ÷ S[minelem, index]
-    S[maxelem, :] -= multiple * S[minelem, :]
-    L[maxelem, :] -= multiple * L[minelem, :]
-  end
-  if S[index, index] == 0
-    k = findfirst(x -> x != 0, S[:, index])
-    k == 0 && error("Internal error ", S[:, index])
-    S[k, :], S[index, :] = copy(S[index, :]), copy(S[k, :])
-    L[k, :], L[index, :] = copy(L[index, :]), copy(L[k, :])
-  end
-  if S[index, index] < 0
-    S[index, :] *= -1
-    L[index, :] *= -1
-  end
-  L, S
-end
-
-function smith_row_impl(smith::Matrix, right::Matrix, index::Int)
-  R, S = smith_col_impl(transpose(right), transpose(smith), index)
-  transpose(S), transpose(R)
-end
-
-function smith_normal_form{T <: Integer}(matrix::Matrix{T})
-  det(matrix) == 0 && error("Input matrix is singular")
-  size(matrix, 1) != size(matrix, 2) && error("Input matrix is not square")
-  smith = copy(matrix)
-  left = eye(eltype(smith), 3)
-  right = eye(eltype(smith), 3)
-  for index in 1:size(smith, 1)
-    once = true
-    while once || !check_nonzero(smith, index)
-      once = false
-      left, smith = smith_col_impl(left, smith, index)
-      smith, right = smith_row_impl(smith, right, index)
-
-      const diag = smith[index, index]
-      maxrow, maxmod = 0, 0
-      for  i = index + 1:size(smith, 1), j = index + 1: size(smith, 2)
-        smith[i, j] % diag == 0 && continue
-        if maxmod == 0 || abs(smith[i, j] % diag) > maxmod
-          maxrow = i
-          maxmod = abs(smith[i, j] % diag)
-        end
-      end
-      if maxmod != 0
-        smith[index, :] += smith[maxrow, :]
-        left[index, :] += left[maxrow, :]
-      end
-    end
-  end
-  if smith[end, end] < 0
-    smith[end, :] *= -1
-    left[end, :] *= -1
-  end
-  left, smith, right
-end
 end
--- test/SNF.jl
+++ test/SNF.jl
@@ -1,19 +1,65 @@
 facts("Smith Normal Form") do
-  context("Implementation functions") do
-    get_min_max = Crystals.SNF.get_min_max
-    for i in 1:10
-      sequence = shuffle(collect(0:10))
-      min = findfirst(x -> x == 1, sequence)
-      max = findfirst(x -> x == 10, sequence)
-      @fact get_min_max(sequence) --> (min, max)
+  context("choose pivot") do
+
+    context("With zero columns") do
+      smith = [0 4 4; 0 6 12; 0 -4 -16]
+      left = eye(eltype(smith), 3)
+      jt = Crystals.SNF.choose_pivot!(left, smith, 1, 1)
+      @fact jt --> 2
+      @fact smith --> [0 4 4; 0 6 12; 0 -4 -16]
+      @fact left --> eye(eltype(smith), 3)
+    end
+
+    context("Switch rows") do
+      smith = [0 0 4; 0 6 12; 0 -4 -16]
+      left = eye(eltype(smith), 3)
+      jt = Crystals.SNF.choose_pivot!(left, smith, 1, 1)
+      @fact jt --> 2
+      @fact smith --> [0 6 12; 0 0 4; 0 -4 -16]
+      @fact left --> [0 1 0; 1 0 0; 0 0 1]
+      @fact left * smith --> [0 0 4; 0 6 12; 0 -4 -16]
     end
   end
-  facts("Random Matrices") do
-    for test in 1:1
-      matrix = rand(-4:4, (3, 3))
-      left, S, right = smith_normal_form(matrix)
-      @fact diagm(diag(S)) --> S
-      @fact left * S * transpose(right) --> matrix
+
+  context("Improve col pivot") do
+    context("All multiples") do
+      smith = [1 0 4; 0 2 12; 0 -4 -16]
+      left = eye(eltype(smith), 3)
+      Crystals.SNF.improve_col_pivot!(left, smith, 1, 1)
+      @fact left --> eye(eltype(smith), 3)
+      @fact smith --> [1 0 4; 0 2 12; 0 -4 -16]   
+
+      Crystals.SNF.improve_col_pivot!(left, smith, 2, 2)
+      @fact left --> eye(eltype(smith), 3)
+      @fact smith --> [1 0 4; 0 2 12; 0 -4 -16]   
     end
+
+    context("Create multiple") do
+      smith = [1 0 4; 0 3 12; 0 -4 -16]
+      left = eye(eltype(smith), 3)
+      Crystals.SNF.improve_col_pivot!(left, smith, 2, 2)
+      @fact smith[:, 2] .% smith[2, 2] .== 0 --> all
+      @fact left * smith --> [1 0 4; 0 3 12; 0 -4 -16]   
+    end
+  end
+
+  context("All multiples") do
+    smith = transpose([1 0 4; 0 2 12; 0 -4 -16])
+    right = eye(eltype(smith), 3)
+    Crystals.SNF.improve_row_pivot!(smith, right, 1, 1)
+    @fact right --> eye(eltype(smith), 3)
+    @fact smith --> transpose([1 0 4; 0 2 12; 0 -4 -16])
+
+    Crystals.SNF.improve_row_pivot!(smith, right, 2, 2)
+    @fact right --> eye(eltype(smith), 3)
+    @fact smith --> transpose([1 0 4; 0 2 12; 0 -4 -16]   )
+  end
+
+  context("Create multiple") do
+    smith = transpose([1 0 4; 0 3 12; 0 -4 -16])
+    right = eye(eltype(smith), 3)
+    Crystals.SNF.improve_row_pivot!(smith, right, 2, 2)
+    @fact smith[2, :] .% smith[2, 2] .== 0 --> all
+    @fact smith * right --> transpose([1 0 4; 0 3 12; 0 -4 -16])
   end
 end
