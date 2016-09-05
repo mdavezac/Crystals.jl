b0fdc45bb3840ddf8a377cd732f175505e9c6d60
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-09-05 11:55:49 +0100

    Incorrect order in fact check

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using DataFrames: nrow, ncol, NA, DataArray, DataFrame, deleterows!, NA,
 contains(x) = y -> x ∈ y
 all_integers(x::Array, ε::AbstractFloat=1e-8) = all(abs(x - round(Integer, x)) .< ε)
 all_integers(ε::AbstractFloat=1e-8) = y -> all_integers(y, ε)
-is_subtype(x::Type) = y -> (x <: y)
+is_subtype(x::Type) = y -> (y <: x)
 
 include("Positions.jl")
 include("Crystal.jl")
